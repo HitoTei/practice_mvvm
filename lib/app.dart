5753f31a0911334d31114d0191e55eea0bf392a8
HitoTei <ttnaoto@gmail.com>
2022-01-20 09:12:10 +0900

    fontを変更

--- lib/app.dart
+++ lib/app.dart
@@ -17,10 +17,22 @@ class _App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final darkMode = Provider.of<bool>(context);
+    final darkTextTheme =
+        ThemeData.dark().textTheme.apply(fontFamily: 'Noto Sans JP');
+    final lightTextTheme =
+        ThemeData.light().textTheme.apply(fontFamily: 'Noto Sans JP');
+    final darkTheme = ThemeData.dark().copyWith(
+      textTheme: darkTextTheme,
+      primaryTextTheme: darkTextTheme,
+    );
+    final lightTheme = ThemeData.light().copyWith(
+      textTheme: lightTextTheme,
+      primaryTextTheme: lightTextTheme,
+    );
     return MaterialApp(
       title: 'Novel Writer',
-      theme: darkMode ? ThemeData.dark() : ThemeData.light(),
-      darkTheme: ThemeData.dark(),
+      theme: darkMode ? darkTheme : lightTheme,
+      darkTheme: darkTheme,
       home: HomePage(),
     );
   }
